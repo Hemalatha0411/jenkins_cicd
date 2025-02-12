e839abcd4801d58601ca9d3e3ceaa6a095937081
Hemalatha0411 <140187972+Hemalatha0411@users.noreply.github.com>
2025-02-12 14:19:16 +0530

    Update App.java with data

--- src/main/java/com/app/example/App.java
+++ src/main/java/com/app/example/App.java
@@ -20,6 +20,6 @@ public class App extends SpringBootServletInitializer {
 
    @RequestMapping(value = "/")
    public String hello() {
-      return "<center>Hello World Aahhh Mantaapp</center>";
+      return "<center>Deploying Java Application with Tomcat using Jenkins CI-CD</center>";
    }
 }
